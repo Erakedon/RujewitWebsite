71005267ec1c4ec2b6f24f9dab6497fa74381801
Erakedon <erakedon@gmail.com>
2019-02-17 13:39:24 +0100

    project initialized

---
+++ src/pages/Team/Team.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Team extends Component {
+    state = {  }
+    render() { 
+        return (
+            <h1>This is Team Page</h1>
+          );
+    }
+}
+ 
+export default Team;
