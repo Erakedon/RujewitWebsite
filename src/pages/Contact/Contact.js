0f1f30379a1fa1b23e9993e3e3406085fd932645
Erakedon <erakedon@gmail.com>
2019-08-16 00:39:29 +0200

    Code cleaning and deleting unused files

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -5,22 +5,18 @@ class Contact extends Component {
     render() { 
         return ( 
             <div className="Contact">
-
                 <div className="singleInfo">
                     <div className="name">Telefon:</div>
                     <a className="data" href="tel:781-096-331">781 096 331</a>
                 </div>
-
                 <div className="singleInfo">
                     <div className="name">E-mail:</div>
                     <a className="data" href="mailto:dnrujewit@gmail.com">dnrujewit@gmail.com</a>
-                </div>
-                
+                </div>                
                 <div className="singleInfo">
                     <div className="name">Rekrutacja:</div>
                     <a className="data" href="mailto:rekrutacja@rujewit.pl">rekrutacja@rujewit.pl</a>
                 </div>
-
             </div>
          );
     }
